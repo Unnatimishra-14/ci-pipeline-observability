0dcb39c28a4ffc4216f7ca075f80c4b9ff687736
Unnati Mishra <unnatim521@gmail.com>
2024-08-01 12:48:42 +0530

    updated ci.yml

--- trace.sh
+++ trace.sh
@@ -5,7 +5,7 @@ curl -L https://github.com/open-telemetry/opentelemetry-go-instrumentation/relea
 chmod +x opentelemetry-instrument
 
 # Set up OpenTelemetry exporter
-export OTEL_EXPORTER_JAEGER_ENDPOINT=http://<ngrok-url>/api/traces
+export OTEL_EXPORTER_JAEGER_ENDPOINT=http://https://ff9b-2401-4900-1c83-b5c4-48f0-79a2-9814-9a0c.ngrok-free.app/search/api/traces
 export OTEL_SERVICE_NAME=github-actions-pipeline
 
 # Run the command with OpenTelemetry instrumentation
