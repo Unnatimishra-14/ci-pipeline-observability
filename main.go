d153df06b490eb7fcb36eb1009b98726e84b90e7
Unnati Mishra <unnatim521@gmail.com>
2024-08-01 14:45:16 +0530

    modified files

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
-// main.go
 package main
 
 import (
-	"fmt"
-	"time"
+    "context"
+    "fmt"
+    "os"
+    "time"
+
+    "go.opentelemetry.io/otel"
+    "go.opentelemetry.io/otel/exporters/jaeger"
+    "go.opentelemetry.io/otel/sdk/resource"
+    sdktrace "go.opentelemetry.io/otel/sdk/trace"
+    "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
+func initTracer() func() {
+    exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(os.Getenv("OTEL_EXPORTER_JAEGER_ENDPOINT"))))
+    if err != nil {
+        fmt.Printf("Failed to create Jaeger exporter: %v\n", err)
+        return func() {}
+    }
+
+    tp := sdktrace.NewTracerProvider(
+        sdktrace.WithBatcher(exporter),
+        sdktrace.WithResource(resource.NewWithAttributes(
+            semconv.SchemaURL,
+            semconv.ServiceNameKey.String("github-actions-pipeline"),
+        )),
+    )
+
+    otel.SetTracerProvider(tp)
+
+    return func() {
+        _ = tp.Shutdown(context.Background())
+    }
+}
+
 func main() {
-	fmt.Println("Starting the application...")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Application finished.")
+    shutdown := initTracer()
+    defer shutdown()
+
+    tracer := otel.Tracer("example-tracer")
+    _, span := tracer.Start(context.Background(), "main")
+    defer span.End()
+
+    fmt.Println("Starting the application...")
+    time.Sleep(5 * time.Second)
+    fmt.Println("Application finished.")
 }
