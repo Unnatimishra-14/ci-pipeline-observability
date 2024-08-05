fb4667d354aa2ad6caea5f7a9355095032e30ac4
Unnati Mishra <unnatim521@gmail.com>
2024-08-05 11:35:49 +0530

    adding files

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "time"
-)
+import "fmt"
 
 func main() {
-    fmt.Println("Starting pipeline simulation")
-
-    // Simulating pipeline steps
-    steps := []string{"Checkout code", "Set up Go", "Install dependencies", "Build application", "Run application"}
-    for _, step := range steps {
-        fmt.Printf("Executing step: %s\n", step)
-        time.Sleep(time.Second) // Simulate some work
-        fmt.Printf("Completed step: %s\n", step)
-    }
-
-    fmt.Println("Pipeline simulation completed")
+    fmt.Println("Hello, World!")
 }
